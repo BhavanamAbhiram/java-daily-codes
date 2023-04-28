b6864fbff5ad540107116ce89bf0a65be1174544
BhavanamAbhiram <125419694+BhavanamAbhiram@users.noreply.github.com>
2023-04-28 22:44:29 +0530

    Week2 Day2 Ques1.java

---
+++ Week2 Day2 Ques1.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+
+    public static void main(String[] args)
+    {
+        Scanner abhi=new Scanner(System.in);
+        int a=abhi.nextInt();
+        float b=a;
+        
+        if(b<0)
+        {
+            System.out.println("Invalid Input");
+}
+        else
+        {
+            System.out.println(b);
+}
+    }
+}
