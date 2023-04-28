cb92a5ab45a6338a03336f60841e08120d5ee43d
BhavanamAbhiram <125419694+BhavanamAbhiram@users.noreply.github.com>
2023-04-28 22:40:17 +0530

    WEEK2 DAY1 Q2.java

---
+++ WEEK2 DAY1 Q2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner abhi=new Scanner(System.in);
+        int a=abhi.nextInt();
+        int b=abhi.nextInt();
+        int c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
