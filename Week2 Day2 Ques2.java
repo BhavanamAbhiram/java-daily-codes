798ec17a5283725e7967cc1d1522aef1770c4915
BhavanamAbhiram <125419694+BhavanamAbhiram@users.noreply.github.com>
2023-04-28 22:46:02 +0530

    Week2 Day2 Ques2.java

---
+++ Week2 Day2 Ques2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+    Scanner abhi=new Scanner(System.in);
+  float r;
+    r=abhi.nextFloat();
+        if(r>0&&r<5)
+        {
+    System.out.println("Thanks");
+  
+        }
+        else
+        {
+                System.out.println("Error: 101");
+
+        }
+    }
+}
