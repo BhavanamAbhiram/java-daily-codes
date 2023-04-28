df7eb9a6dd0bbab30dd5914ef7997b2f1735b86f
BhavanamAbhiram <125419694+BhavanamAbhiram@users.noreply.github.com>
2023-04-28 22:53:28 +0530

    Week2 Day5 Ques1.java

---
+++ Week2 Day5 Ques1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sai= new Scanner(System.in);
+        int x=(int)Math.ceil(sai.nextDouble());
+        int y=(int)Math.ceil(sai.nextDouble());
+        int z=(int)Math.ceil(sai.nextDouble());
+        System.out.print(x+" "+y+ " "+z+" ");
+    }
+}
+   
